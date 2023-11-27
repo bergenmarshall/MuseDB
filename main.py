47baa6041c11b2e8a9b93d6605f76069a4fe07cf
Bergen Marshall <bergenmarshall34@gmail.com>
2023-11-26 18:22:55 -0600

    Create Initial Login and Register Endpoints

--- main.py
+++ main.py
@@ -6,3 +6,13 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.post("/login")
+async def login(username: str, password: str):
+    if username == "admin" and password == "admin":
+        return {"username": username}
+    return {"username": "unknown"}
+
+@app.post("/register")
+async def register(username: str, password: str):
+    return {"username": username, "password": password}
