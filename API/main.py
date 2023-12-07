2d1d88915decfc6a17e418708c73c0d84595086e
Bergen Marshall <bergenmarshall34@gmail.com>
2023-12-06 21:19:11 -0600

    fixed bug where artists without pictures crashed

--- API/main.py
+++ API/main.py
@@ -51,14 +51,14 @@ async def register(username: str, password: str):
 @app.get("/search")
 async def search(query: str, search_type: str):
     query = query.replace(" ", "")
-    print(query, search_type)
     if search_type not in ['track', 'album', 'artist']:
         raise HTTPException(status_code=404, detail="Not a valid search type")
     access_token = await get_token()
     response = get(f"https://api.spotify.com/v1/search?q={query}&type={search_type}&limit=5", headers={"Authorization": f"Bearer {access_token}"})
     if response.status_code != 200:
         raise HTTPException(status_code=500, detail="Unable to get search results")
-    
+    if response.json()[search_type + "s"]["total"] == 0:
+        raise HTTPException(status_code=404, detail="No results found")
     return_val = {}
     if search_type == "track":
         for i in response.json()["tracks"]["items"]:
@@ -68,6 +68,8 @@ async def search(query: str, search_type: str):
             return_val[i["id"]] = {"name": i["name"], "artist": i["artists"][0]["name"], "image": i["images"][0]["url"]}
     elif search_type == "artist":
         for i in response.json()["artists"]["items"]:
+            if len(i["images"]) == 0:
+                continue
             return_val[i["id"]] = {"name": i["name"], "image": i["images"][0]["url"]}
     return return_val
 
